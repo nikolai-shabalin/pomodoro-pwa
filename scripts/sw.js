daf9f52b3e0eaf86c33224955ea4c6340e007c64
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-02-08 22:03:55 +0300

    Test path

--- scripts/sw.js
+++ scripts/sw.js
@@ -2,12 +2,11 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('pomodoro-cache-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles/styles.css',
-        '/scripts/app.js',
-        '/images/play.svg',
-        '/images/pause.svg'
+        '../index.html',
+        '../styles/styles.css',
+        '../scripts/app.js',
+        '../images/play.svg',
+        '../images/pause.svg'
       ]);
     })
   );
